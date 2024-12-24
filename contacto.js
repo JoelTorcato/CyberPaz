35b17da6a6aac90fbc560a038582f1ec9de3eee5
Dev Joel <a12022@ae4outubro.pt>
2024-12-24 10:09:47 +0000

    I've been reviewing the JavaScript code and making comments

--- contacto.js
+++ contacto.js
@@ -1,13 +1,13 @@
 
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => { // Runs script after webpage content fully loads
     const inputs = document.querySelectorAll("input, textarea");
   
-    inputs.forEach((input) => {
-      input.addEventListener("input", () => {
+    inputs.forEach((input) => { // Loops through each input or textarea field (Arrays and collections)
+      input.addEventListener("input", () => { // Function
         if (input.checkValidity() && input.value.trim() !== "") {
           input.style.backgroundColor = "#DFF2BF";
         } else {
-          input.style.backgroundColor = "";
+          input.style.backgroundColor = "#";
         }
       });
     });
